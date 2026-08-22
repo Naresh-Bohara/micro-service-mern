b8b63b3d7faee9f65c091880baf7a5ab11fd7ffc
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-23 00:15:29 +0545

    updated

--- bullmq/worker.js
+++ bullmq/worker.js
@@ -1,1 +1,25 @@
-const {Worker} = require("bullmq");
+const { Worker } = require("bullmq");
+
+const sendEmail = () =>
+    new Promise((resolve) =>
+        setTimeout(() => resolve(), 5 * 1000)
+    );
+
+const worker = new Worker(
+    "email-queue",
+    async (job) => {
+        console.log(`Message received with id ${job.id}`);
+        console.log(`Processing message`);
+        console.log(`Sending email to ${job.data.email}`);
+
+        await sendEmail();
+
+        console.log("email sent!");
+    },
+    {
+        connection: {
+            host: "localhost",
+            port: 6379
+        }
+    }
+);
